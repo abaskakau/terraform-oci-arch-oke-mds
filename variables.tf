65f6ee1d5247f96ad75876fda63dafe1de381ed6
Andrey Baskakov <andrei.baskakau@softswiss.com>
2022-11-09 12:01:32 +0400

    Remove mysql

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-## Copyright (c) 2022, Oracle and/or its affiliates. 
+## Copyright (c) 2022, Oracle and/or its affiliates.
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
 variable "tenancy_ocid" {}
@@ -103,52 +103,6 @@ variable "node_pool_flex_shape_ocpus" {
 
 variable "cluster_name" {
   default = "okemdscluster"
-}
-
-variable "admin_password" {
-  description = "Password for the admin user for MySQL Database Service"
-}
-
-variable "admin_username" {
-  description = "MySQL Database Service Username"
-  default     = "admin"
-}
-
-variable "mysql_shape" {
-  default = "MySQL.VM.Standard.E3.1.8GB"
-}
-
-variable "mysql_db_system_data_storage_size_in_gb" {
-  default = 50
-}
-
-variable "mysql_db_system_description" {
-  description = "MySQL DB System description"
-  default     = "MySQL DB System description"
-}
-
-variable "mysql_db_system_display_name" {
-  description = "MySQL DB System display name"
-  default     = "OKE-MDS"
-}
-
-variable "mysql_db_system_fault_domain" {
-  description = "The fault domain on which to deploy the Read/Write endpoint. This defines the preferred primary instance."
-  default     = "FAULT-DOMAIN-1"
-}
-
-variable "mysql_db_system_hostname_label" {
-  description = "The hostname for the primary endpoint of the DB System. Used for DNS. The value is the hostname portion of the primary private IP's fully qualified domain name (FQDN) (for example, dbsystem-1 in FQDN dbsystem-1.subnet123.vcn1.oraclevcn.com). Must be unique across all VNICs in the subnet and comply with RFC 952 and RFC 1123."
-  default     = "okemds"
-}
-
-variable "mysql_db_system_maintenance_window_start_time" {
-  description = "The start of the 2 hour maintenance window. This string is of the format: {day-of-week} {time-of-day}. {day-of-week} is a case-insensitive string like mon, tue, etc. {time-of-day} is the Time portion of an RFC3339-formatted timestamp. Any second or sub-second time data will be truncated to zero."
-  default     = "SUNDAY 14:30"
-}
-
-variable "mysql_is_highly_available" {
-  default = false
 }
 
 # Dictionary Locals
